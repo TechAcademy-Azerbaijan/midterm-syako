5172a1e3ec6e5e63f7b8396fdf1ed582192cb63b
Sarkhan <syako44@gmail.com>
2022-06-21 19:17:26 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,11 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    var number = parseInt(result.input); // 9
+    var square_number = number*number; // 81
+    console.log(square_number);
 });
