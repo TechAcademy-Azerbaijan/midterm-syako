92fd2508ffb12451601fd367c94d6861af20953d
Sarkhan <syako44@gmail.com>
2022-06-21 19:09:06 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,15 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    var n = parseInt(result.input); // 9
+    let res = 0
+while(n > 0){
+    res = res + n % 10
+    n = parseInt(n / 10)
+
+}console.log(res);
 });
